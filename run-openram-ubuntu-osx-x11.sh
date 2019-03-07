35634862e221e2e5c17012be283bb564c5009599
Matt Guthaus <mrg+openram@ucsc.edu>
2019-03-07 11:03:56 -0800

    Fix docker runs to runlatest

--- run-openram-ubuntu-osx-x11.sh
+++ run-openram-ubuntu-osx-x11.sh
@@ -2,4 +2,4 @@
 ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')                                                                                                 
 xhost + $ip                                                                                                                                                  
 display_number=`ps -ef | grep "Xquartz :\d" | grep -v xinit | awk '{ print $9; }'`                                                                           
-docker run -it -e LOCAL_USER=$USER -e LOCAL_HOME=$HOME -v $HOME:${HOME} -v $HOME/bsoe/software:/software -e DISPLAY=$ip$display_number -v /tmp/.X11-unix:/tmp/.X11-unix vlsida/openram-ubuntu            
+docker run -it -e LOCAL_USER=$USER -e LOCAL_HOME=$HOME -v $HOME:${HOME} -v $HOME/bsoe/software:/software -e DISPLAY=$ip$display_number -v /tmp/.X11-unix:/tmp/.X11-unix vlsida/openram-ubuntu:latest 
