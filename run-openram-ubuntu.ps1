17253d416426f0ab9c7fb5d53642095d1f6b4da2
Jesse Cirimelli-Low <jcirimel@ucsc.edu>
2019-03-11 15:36:23 -0700

    Use ip for X display server

--- run-openram-ubuntu.ps1
+++ run-openram-ubuntu.ps1
@@ -1,2 +1,4 @@
 
-docker run -it -e LOCAL_USER=$env:UserName -e LOCAL_HOME=/home/$env:UserName -v ~:/home/$env:UserName -v ~/bsoe/software:/software -e DISPLAY="$env:ComputerName":0.0 vlsida/openram-ubuntu:latest
+$ipV4 = Test-Connection -ComputerName (hostname) -Count 1  | Select -ExpandProperty IPV4Address
+$ipV4 = $ipV4.IPAddressToString
+docker run -it -e LOCAL_USER=$env:UserName -e LOCAL_HOME=/home/$env:UserName -v ~/bsoe/software:/software -v ~:/home/$env:UserName  -e DISPLAY="$ipV4":0.0 vlsida/openram-ubuntu:latest
