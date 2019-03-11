604352215a1fd6bcc75d1b63046b953438a55a8c
Matthew Guthaus <mrg@ucsc.edu>
2019-03-11 08:58:15 -0700

    Update display in powershell script

--- run-openram-ubuntu.ps1
+++ run-openram-ubuntu.ps1
@@ -1,2 +1,2 @@
 
-docker run -it -e LOCAL_USER=$env:UserName -e LOCAL_HOME=/home/$env:UserName -v ~:/home/$env:UserName -v ~/bsoe/software:/software -e DISPLAY=192.168.1.100:0.0 vlsida/openram-ubuntu:latest
+docker run -it -e LOCAL_USER=$env:UserName -e LOCAL_HOME=/home/$env:UserName -v ~:/home/$env:UserName -v ~/bsoe/software:/software -e DISPLAY="$env:ComputerName":0.0 vlsida/openram-ubuntu:latest
