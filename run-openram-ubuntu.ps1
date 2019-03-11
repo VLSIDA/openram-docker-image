1ef8446525a1eab7d989fb4628b98c549526b4e5
Jesse Cirimelli-Low <jcirimel@ucsc.edu>
2019-03-11 15:58:40 -0700

    make username lowercase

--- run-openram-ubuntu.ps1
+++ run-openram-ubuntu.ps1
@@ -1,4 +1,5 @@
 
 $ipV4 = Test-Connection -ComputerName (hostname) -Count 1  | Select -ExpandProperty IPV4Address
 $ipV4 = $ipV4.IPAddressToString
-docker run -it -e LOCAL_USER=$env:UserName -e LOCAL_HOME=/home/$env:UserName -v ~/bsoe/software:/software -v ~:/home/$env:UserName  -e DISPLAY="$ipV4":0.0 vlsida/openram-ubuntu:latest
+$USER = $env:UserName.ToLower()
+docker run -it -e LOCAL_USER=$USER -e LOCAL_HOME=/home/$env:UserName -v ~/bsoe/software:/software -v ~:/home/$env:UserName  -e DISPLAY="$ipV4":0.0 vlsida/openram-ubuntu:latest
