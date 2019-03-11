15935ed8916c845edfeb5a0a0f18d35797fc78e0
Jesse Cirimelli-Low <jcirimel@ucsc.edu>
2019-03-11 16:00:43 -0700

    updated mounting paths to be consistant with lowercase username

--- run-openram-ubuntu.ps1
+++ run-openram-ubuntu.ps1
@@ -2,4 +2,4 @@
 $ipV4 = Test-Connection -ComputerName (hostname) -Count 1  | Select -ExpandProperty IPV4Address
 $ipV4 = $ipV4.IPAddressToString
 $USER = $env:UserName.ToLower()
-docker run -it -e LOCAL_USER=$USER -e LOCAL_HOME=/home/$env:UserName -v ~/bsoe/software:/software -v ~:/home/$env:UserName  -e DISPLAY="$ipV4":0.0 vlsida/openram-ubuntu:latest
+docker run -it -e LOCAL_USER=$USER -e LOCAL_HOME=/home/$USER -v ~/bsoe/software:/software -v ~:/home/$USER  -e DISPLAY="$ipV4":0.0 vlsida/openram-ubuntu:latest
